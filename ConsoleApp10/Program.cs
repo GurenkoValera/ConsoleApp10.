434494045e6dcca6f2d73c42301e44b284bc88a5
valer <valer@DESKTOP-QD94NVR>
2024-10-06 19:13:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод данных из таблицы
+        Console.Write("Введите название передачи: ");
+        string nazvanie = Console.ReadLine();
+        Console.Write("Введите имя ведущего: ");
+        string vedushiy = Console.ReadLine();
+        Console.Write("Введите рейтинг передачи: ");
+        int reiting = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите тип передачи (И, А, Т): ");
+        string tip = Console.ReadLine();
+
+        // Вывод форматированной таблицы на экран
+        Console.WriteLine("Телепередачи");
+        Console.WriteLine("------------");
+        Console.WriteLine("Передача\tВедущий\tРейтинг\tТип");
+        Console.WriteLine("------------");
+        Console.WriteLine($"{nazvanie}\t{vedushiy}\t{reiting}\t{tip}");
+        Console.WriteLine("------------");
+        Console.WriteLine("Примечания:");
+        Console.WriteLine("И - игровая");
+        Console.WriteLine("А - аналитическая");
+        Console.WriteLine("Т - ток-шоу");
+    }
+}
